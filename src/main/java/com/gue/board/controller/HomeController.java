882628c85275dc8eed91c614f0efa5e479d4f188
user <rbdus9696@naver.com>
2023-07-17 18:29:37 +0900

    2023.07.17 김규연 1. 룰렛 오류사항 수정

--- src/main/java/com/gue/board/controller/HomeController.java
+++ src/main/java/com/gue/board/controller/HomeController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping(value = "/index")
+    @GetMapping(value = "/roulette")
     public String home() {
-        return "rollet.html";
+        return "roulette.html";
+    }
+
+    @GetMapping(value = "/index")
+    public String index() {
+        return "index.html";
+    }
+
+    @GetMapping(value = "/ladder")
+    public String ladder() {
+        return "ladder.html";
+    }
+
+    @GetMapping(value = "/board")
+    public String board() {
+        return "board.html";
     }
 }
